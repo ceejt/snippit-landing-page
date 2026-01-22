9efb7cc4ce07679e2ea4730ed67b9b0e5021fb2b
Cyril Josef Tinae <cyriljoseft@gmail.com>
2026-01-22 18:38:57 +0800

    Omitted other component imports for hero section testing.

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,9 @@
-import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import Features from "./components/Features";
-import Demo from "./components/Demo";
-import Testimonials from "./components/Testimonials";
-import CTA from "./components/CTA";
-import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Hero />
-      <Features />
-      <Demo />
-      <Testimonials />
-      <CTA />
-      <Footer />
     </div>
   );
 }
